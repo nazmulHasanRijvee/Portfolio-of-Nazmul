ba43ed660c23b6e776714d0b8d8fc915e8e4554b
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-07-08 11:22:44 +0600

    Optimized skill_section.dart

--- lib/features/home/widgets/skill_section.dart
+++ lib/features/home/widgets/skill_section.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter7_portfolio/core/constants/app_colors.dart';
-import 'package:flutter7_portfolio/core/constants/app_text_styles.dart';
-import 'package:flutter7_portfolio/data/models/skill_section_model.dart';
-import 'package:flutter7_portfolio/features/home/widgets/section_header.dart';
 
 import '../../../core/constants/app_breakpoints.dart';
+import '../../../core/constants/app_colors.dart';
 import '../../../core/constants/app_strings.dart';
+import '../../../core/constants/app_text_styles.dart';
+import '../../../data/models/skill_section_model.dart';
+import 'section_header.dart';
 
 class SkillSection extends StatefulWidget {
-  const SkillSection({super.key});
+  const SkillSection({super.key}); // _skillsKey is directly assigned
 
   @override
   State<SkillSection> createState() => _SkillSectionState();
@@ -16,6 +16,7 @@ class SkillSection extends StatefulWidget {
 
 class _SkillSectionState extends State<SkillSection> {
 
+  // ValueNotifier to detect mouse pointer hovering
   late final ValueNotifier<int> currentIndex;
 
   @override
@@ -26,7 +27,7 @@ class _SkillSectionState extends State<SkillSection> {
 
   @override
   void dispose() {
-    currentIndex.dispose();
+    currentIndex.dispose(); // must dispose
     super.dispose();
   }
 
@@ -50,6 +51,7 @@ class _SkillSectionState extends State<SkillSection> {
 
   }
 
+  /// For desktop
   Widget buildDesktopLayout(double width) {
 
     final double ratio = (width / AppBreakpoints.desktop).clamp(0.5, 1.0);
@@ -60,6 +62,7 @@ class _SkillSectionState extends State<SkillSection> {
         crossAxisAlignment: .start,
         mainAxisSize: .min,
         children: [
+          // Section header to show section number & title
           SectionHeader(
               ratio: ratio,
               isMobile: false,
@@ -82,6 +85,7 @@ class _SkillSectionState extends State<SkillSection> {
 
   }
 
+  /// For tablet
   Widget buildTabletLayout(double width) {
 
     final double ratio = (width / AppBreakpoints.tablet).clamp(0.5, 1.0);
@@ -115,7 +119,7 @@ class _SkillSectionState extends State<SkillSection> {
 
   }
 
-
+  /// For mobile
   Widget buildMobileLayout(double width) {
 
     final double ratio = (width / AppBreakpoints.mobile).clamp(0.5, 1.0);
@@ -142,6 +146,8 @@ class _SkillSectionState extends State<SkillSection> {
 
   }
 
+
+  /// build grid of skills for desktop & tablet
   GridView buildSkillGrid({
     required double ratio,
     required int crossAxisCount,
@@ -166,67 +172,26 @@ class _SkillSectionState extends State<SkillSection> {
 
           final model = SkillSectionModel.skillModels[index];
 
-          return MouseRegion(
-              onEnter: (event) {
-                currentIndex.value = index;
-              },
-              onExit: (event) {
-                currentIndex.value = 9;
-              },
-              child: ValueListenableBuilder(
-                  valueListenable: currentIndex,
-                  builder: (context, int value, child) {
-                    return buildSkillContainer(
-                        ratio: ratio,
-                        model: model,
-                        isHovered:  index == value,
-                        isTablet: isTablet,
-                    );
-                  }
-              )
+          return buildMouseRegion(
+            index: index,
+            child: ValueListenableBuilder(
+                valueListenable: currentIndex,
+                builder: (context, int value, child) {
+                  return buildSkillContainer(
+                      ratio: ratio,
+                      model: model,
+                      isHovered:  index == value,
+                      isTablet: isTablet,
+                  );
+                }
+            ),
           );
 
         }
     );
   }
 
-  Widget buildSkillListView(double ratio) {
-
-    return ListView.separated(
-        clipBehavior: .none,
-        shrinkWrap: true,
-        padding: .zero,
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: SkillSectionModel.skillModels.length,
-        itemBuilder: (BuildContext context, int index) {
-
-          final model = SkillSectionModel.skillModels[index];
-
-          return MouseRegion(
-              onEnter: (event) {
-                currentIndex.value = index;
-              },
-              onExit: (event) {
-                currentIndex.value = 9;
-              },
-              child: ValueListenableBuilder(
-                  valueListenable: currentIndex,
-                  builder: (context, int value, child) {
-                    return buildSkillContainerForMobile(
-                      ratio: ratio,
-                      model: model,
-                      isHovered:  index == value,
-                    );
-                  }
-              )
-          );
-
-        },
-        separatorBuilder: (context, index) => const SizedBox(height: 15),
-    );
-
-  }
-
+  // build skill container for buildSkillGrid()
   Widget buildSkillContainer({
     required double ratio,
     required SkillSectionEntity model ,
@@ -239,12 +204,12 @@ class _SkillSectionState extends State<SkillSection> {
       alignment: .centerStart,
 
       decoration: BoxDecoration(
-        color: AppColors.skillContainer,
-        borderRadius: BorderRadiusGeometry.circular(6),
-        border: isHovered ? Border.all(
-          color: AppColors.primaryColor.withValues(alpha: 0.5),
-          width: 0.5
-        ) : null
+          color: AppColors.skillContainer,
+          borderRadius: BorderRadiusGeometry.circular(6),
+          border: isHovered ? Border.all(
+              color: AppColors.primaryColor.withValues(alpha: 0.5),
+              width: 0.5
+          ) : null
       ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -275,37 +240,86 @@ class _SkillSectionState extends State<SkillSection> {
 
   }
 
+  // build skill row for buildSkillContainer()
   Widget buildSkillRow({ required double ratio, required List<String> skills, bool isTablet = false}){
 
     return SizedBox(
       width: isTablet ? 400 * ratio : 200 * ratio,
       child: Wrap(
 
-        spacing: isTablet ? 15 * ratio : 10 * ratio,
-        runSpacing: isTablet ? 15 * ratio : 10 * ratio,
+          spacing: isTablet ? 15 * ratio : 10 * ratio,
+          runSpacing: isTablet ? 15 * ratio : 10 * ratio,
 
-        children: List.generate(skills.length, (int index){
+          children: List.generate(skills.length, (int index){
 
-          return Container(
-            padding: .symmetric(horizontal: 10 * ratio, vertical: 5 * ratio),
-            decoration: BoxDecoration(
-              color: AppColors.subSkillContainer,
-              borderRadius: BorderRadiusGeometry.circular(3)
-            ),
-            child: Text(
-              skills[index],
-              style: AppTextStyles.subSkillStyle
-                  .copyWith(fontSize: isTablet ? 14 * ratio : 12 * ratio)
-            ),
-          );
+            return Container(
+              padding: .symmetric(horizontal: 10 * ratio, vertical: 5 * ratio),
+              decoration: BoxDecoration(
+                  color: AppColors.subSkillContainer,
+                  borderRadius: BorderRadiusGeometry.circular(3)
+              ),
+              child: Text(
+                  skills[index],
+                  style: AppTextStyles.subSkillStyle
+                      .copyWith(fontSize: isTablet ? 14 * ratio : 12 * ratio)
+              ),
+            );
 
-        })
+          })
 
       ),
     );
 
   }
 
+  // build mouse region for buildSkillListView() and buildSkillGrid()
+  Widget buildMouseRegion({required int index, required Widget child}){
+    return MouseRegion(
+        onEnter: (event) {
+          currentIndex.value = index;
+        },
+        onExit: (event) {
+          currentIndex.value = 9;
+        },
+        child: child
+    );
+  }
+
+  /// build list of skills for mobile
+  Widget buildSkillListView(double ratio) {
+
+    return ListView.separated(
+        clipBehavior: .none,
+        shrinkWrap: true,
+        padding: .zero,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: SkillSectionModel.skillModels.length,
+        itemBuilder: (BuildContext context, int index) {
+
+          final model = SkillSectionModel.skillModels[index];
+
+          return buildMouseRegion(
+            index: index,
+            child: ValueListenableBuilder(
+                valueListenable: currentIndex,
+                builder: (context, int value, child) {
+                  return buildSkillContainerForMobile(
+                    ratio: ratio,
+                    model: model,
+                    isHovered:  index == value,
+                  );
+                }
+            ),
+          );
+
+        },
+        separatorBuilder: (context, index) => const SizedBox(height: 15),
+    );
+
+  }
+
+
+  // build skill container for buildSkillListView()
   Widget buildSkillContainerForMobile({
     required double ratio,
     required SkillSectionEntity model ,
@@ -352,6 +366,7 @@ class _SkillSectionState extends State<SkillSection> {
 
   }
 
+  // build skill row for buildSkillContainerForMobile()
   Widget buildSkillRowForMobile({ required double ratio, required List<String> skills}){
 
     return SizedBox(
